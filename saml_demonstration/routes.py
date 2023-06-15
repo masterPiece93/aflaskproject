d1ecbc1f043b41a1985b35fd5a5dc9dd3f8e6436
Ankit Yadav <42907602+masterPiece93@users.noreply.github.com>
2023-06-15 11:26:23 +0530

    Update routes.py
    added function string to `base` decorator

--- saml_demonstration/routes.py
+++ saml_demonstration/routes.py
@@ -5,6 +5,15 @@ from auth.v2 import auth_bp as auth_bp_v2
 
 
 def base(base_path, **opt_kwargs):
+    """base path , after which actual routing to modules start
+    path = <http|https>://<serving-domain-ip-or-name>/<base-path>/<version>/....
+
+    arguments:
+        base_path:specifies the purpose/section of the route belongin to application
+        opt_kwargs:
+            version:specifies version suffix to base-path .
+                    default value:function name .
+    """
     final_base_path: str = '/{base}/{version}/{value}'
     def _wrapper(func):
         version = opt_kwargs.get('version', func.__name__)
